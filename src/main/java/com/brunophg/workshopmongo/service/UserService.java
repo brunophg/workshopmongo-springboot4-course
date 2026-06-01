e55e8d8484f5ccd5cbbde310b6f5c0b15c31ed9a
brunophg <brunophgjf@gmail.com>
2026-06-01 18:52:33 -0300

    User insertion using POST

--- src/main/java/com/brunophg/workshopmongo/service/UserService.java
+++ src/main/java/com/brunophg/workshopmongo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.brunophg.workshopmongo.service;
 
 import com.brunophg.workshopmongo.domain.User;
+import com.brunophg.workshopmongo.dto.UserDTO;
 import com.brunophg.workshopmongo.repository.UserRepository;
 import com.brunophg.workshopmongo.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
