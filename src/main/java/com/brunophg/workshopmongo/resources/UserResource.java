a5398d0853b79ec7e22e1ce9eb9c23944ffb7ebf
brunophg <brunophgjf@gmail.com>
2026-06-01 12:29:14 -0300

    MongoDB connection with repository and service

---
+++ src/main/java/com/brunophg/workshopmongo/resources/UserResource.java
@@ -0,0 +1,25 @@
+package com.brunophg.workshopmongo.resources;
+
+import com.brunophg.workshopmongo.domain.User;
+import com.brunophg.workshopmongo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
