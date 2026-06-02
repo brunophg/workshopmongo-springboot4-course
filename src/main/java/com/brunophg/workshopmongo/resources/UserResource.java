e33545479f9dd724cc26badce279be1c71e91bf1
brunophg <brunophgjf@gmail.com>
2026-06-02 08:46:17 -0300

    User update using PUT

--- src/main/java/com/brunophg/workshopmongo/resources/UserResource.java
+++ src/main/java/com/brunophg/workshopmongo/resources/UserResource.java
@@ -41,11 +41,18 @@ public class UserResource {
         return ResponseEntity.created(uri).build();
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable String id) {
         service.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
 
 }
