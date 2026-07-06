728964821e04ab49b2947ffbdbbc18256b172bda
brunophg <brunophgjf@gmail.com>
2026-07-06 16:08:46 -0300

    Adding comments to posts

--- src/main/java/com/brunophg/workshopmongo/config/Instantiation.java
+++ src/main/java/com/brunophg/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.brunophg.workshopmongo.config;
 import com.brunophg.workshopmongo.domain.Post;
 import com.brunophg.workshopmongo.domain.User;
 import com.brunophg.workshopmongo.dto.AuthorDTO;
+import com.brunophg.workshopmongo.dto.CommentDTO;
 import com.brunophg.workshopmongo.repository.PostRepository;
 import com.brunophg.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2026"), "Partiu viagem", "Vou viajar, abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2026"), "Good morning", "I'm very happy", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
